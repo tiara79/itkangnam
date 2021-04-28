e1c0f70dad24a41a9984b714973251423d725827
tiara79 <loverjj2@naver.com>
2021-04-29 01:50:54 +0900

    메뉴바 디자인변경,제품 상세페이지 크기 오류 수정

--- static/js/openMenu.js
+++ static/js/openMenu.js
@@ -1,5 +1,23 @@
+
+
+// background-color change
+$(window).on('scroll resize', function(){
+  let scrollTop = $(document).scrollTop();
+  bgColor();
+  function bgColor(){
+    if(scrollTop > 1400){
+      $('body').addClass('on');
+    }else{
+      $('body').removeClass('on');
+    }
+    if(scrollTop > 2700){
+      $('body').removeClass('on');
+    }
+  }
+});
+
 // hamburger 메뉴
-$(document).ready(function(){
+$(function(){
   $('.openMenu button.open').on('click', function(){
       $('.openMenu .menuWrap').addClass('on');
   });
@@ -8,6 +26,14 @@ $(document).ready(function(){
   });
 });
 
-
-
+$(function(){
+  $('.gnb li a').on('mouseenter focus', function(){
+    let bar = $(this).position().left;
+    let widNum = $(this).width();
+    $('span.bar').css({'left':bar + 'px', 'width':widNum + 'px', opacity:1});
+  });
+  $('.gnb li a').on('mouseleave', function(){
+    $('span.bar').css({'left':0, 'width':0, opacity:0});
+  });
+});
 
